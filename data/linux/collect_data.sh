3c98561dacd5b0e62a0b2b1c2f3159cb126496db
hareekshith <hareekshith@gmail.com>
2025-04-16 07:38:20 +0530

    Detects keyloggers which sends real time info without any storage accesses!

--- data/linux/collect_data.sh
+++ data/linux/collect_data.sh
@@ -5,18 +5,22 @@
 mkdir -p samples
 mkdir -p csamples
 #gets the required feed from the computer
-for i in $(seq 1 9);
+for i in $(seq 1 10);
 do
-  sleep 5;
+  sleep 5
   ps -eo pid,comm,%cpu,%mem --sort=-%cpu --no-headers | while read pid comm cpu mem; 
   do
   #Returns the path of the given program
   p=$(readlink -f "/proc/$pid/exe" 2>/dev/null)
   #Checks if the file running is either in log, dat or txt format
-  ai=0
+  ifi=0
   if lsof -p "$pid" 2>/dev/null | grep -E '\.(txt|dat|log)$' > /dev/null; then
-    ai=1;
+    ifi=1;
   fi
-  echo "${pid}|${comm}|${cpu}|${mem}|${p}|${ai}" >> samples/ldata$i.csv
+  ai=0
+  if ls -l /proc/$pid/fd 2>/dev/null | grep -q "/dev/input"; then
+    ai=1
+  fi
+  echo "${pid}|${comm}|${cpu}|${mem}|${p}|${ifi}|${ai}" >> samples/ldata$i.csv
   done
 done
--- data/linux/extract_linfeatures.py
+++ data/linux/extract_linfeatures.py
@@ -12,16 +12,14 @@ def susp(path):
 def sus(r):
     s=0
     if 'p' in r and isinstance(r['p'],str):
-        if susp(r['p']): #checks whether the path is suspicious
+        if 'p' in r and (susp(r['p']) or '/.' in r['p']): #checks whether the path contains /. in path, usually no file should be running
             s+=1
-        if 'p' in r and '/.' in r['p']: #checks whether the path contains /. in path, usually no file should be running
-            s+=1
-        if 'ai' in r and r['ai'] == 1: # checks whether the input is accessed
-            s+=1
+        if int(r.get(ai,0) == 1): #Checks whether suspiciously inputs are written
+            s+=2
     return 1 if s>=2 else 0
 
 def main():
-    f = pd.read_csv("samples/ldata41.csv", sep = "|", names=["pid","process","cpu","memory","p","pid"])
+    f = pd.read_csv("samples/ldata1.csv", sep = "|", names=["pid","process","cpu","memory","ifi","ai"])
     f['label'] = f.apply(sus,axis=1)
     f.to_csv("csamples/flidata.csv", index=False)
     #prints the ones who's label is 1
